9442d8ee08c8f4a1a73420001e85fa425a675cb2
SvetaKuleshova <morfeya.2011@mail.ru>
2023-07-06 02:14:17 +0300

    csv

--- src/test/java/ru/netology/services/Calc/CalculationRestMonthsTest.java
+++ src/test/java/ru/netology/services/Calc/CalculationRestMonthsTest.java
@@ -2,30 +2,17 @@ package ru.netology.services.Calc;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.CalculationRestMonths;
 
 public class CalculationRestMonthsTest {
 
-    @Test
-    void testMethodOfCalculatingMonthsOfRest() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/datacalc.csv")
+    void testMethodOfCalculatingMonthsOfRest(int expected, int income, int expenses, int threshold) {
         CalculationRestMonths service = new CalculationRestMonths();
-
-        int expected = 3;
-
-        int income = 10_000; //доход
-        int expenses = 3_000; //расходы
-        int threshold = 20_000; //порог
-        int rest = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
-
-        for (int month = 0; month < 12; month++)
-            if (money >= threshold) { // можем ли отдыхать?
-                rest++; // увеличиваем счётчик месяцев отдыха
-                money = (money - expenses) / 3;
-            } else {
-                money = money + income - expenses;
-            }
-
 
         int actual = service.calculate(income, expenses, threshold);
 
@@ -34,33 +21,8 @@ public class CalculationRestMonthsTest {
 
     }
 
-    @Test
-    void test2MethodOfCalculatingMonthsOfRest() {
-        CalculationRestMonths service = new CalculationRestMonths();
-
-        int expected = 2;
-
-        int income = 100_000; //доход
-        int expenses = 60_000; //расходы
-        int threshold = 150_000; //порог
-        int rest = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
-
-        for (int month = 0; month < 12; month++)
-            if (money >= threshold) { // можем ли отдыхать?
-                rest++; // увеличиваем счётчик месяцев отдыха
-                money = (money - expenses) / 3;
-            } else {
-                money = money + income - expenses;
-            }
-
-
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
 }
+
 
 
 
